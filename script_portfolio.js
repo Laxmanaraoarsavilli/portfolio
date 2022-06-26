bd55a4803e31f3407aee7184d31685249fafd69d
Laxmanaraoarsavilli <97677641+Laxmanaraoarsavilli@users.noreply.github.com>
2022-06-26 19:14:14 +0530

    Add files via upload

---
+++ script_portfolio.js
@@ -0,0 +1,17 @@
+typed_func=()=>
+{
+    var typed=new Typed ("#dynamic-txts1",{
+        strings: ["Web Developer","Web Designer"],
+        typeSpeed:80,
+        backSpeed:90,
+        loop:true
+    });
+    var typed=new Typed ("#dynamic-txts2",{
+        strings: ["Web Developer","Web Designer"],
+        typeSpeed:80,
+        backSpeed:90,
+        loop:true
+    });
+}
+
+
